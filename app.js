7548061704cd003e082356261c224ba20fce6675
Patelharsh6 <harshpatel010606@gmail.com>
2025-07-15 20:41:35 +0530

    css folder added

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express=require("express");
 const app=express();
+const path=require("path");
+const ejsMate=require("ejs-mate");
+
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
+
 
 const port=3000;
 app.listen(port,()=>{
     console.log(`app is listening at ${port}`);
 })
 
+
+
 app.get("/",(req,res)=>{
-    res.send("request geted");
+    res.render("home.ejs");
 })
